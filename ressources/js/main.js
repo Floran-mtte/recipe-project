e04b58ea8eb3443783afabf1e2c97fe953052427
Floran-mtte <floran77230@gmail.com>
2019-01-09 16:31:11 +0100

    JS et front

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -62,7 +62,12 @@ $(function() {
                     }
                 })
                     .done(function(data, status) {
-                        console.log(data);
+                        $('#nameRecipe').val('');
+                        $('#timeRecipe').val('');
+                        $('.ingredient').each(function(index) {
+                            $(this).val('');
+                        });
+
                     })
                     .fail(function (status, response, error) {
 
@@ -86,14 +91,34 @@ $(function() {
     $('#searchRecipe').on('keyup', function (event) {
        var searchRecipe = $('#searchRecipe').val();
 
+        if (searchRecipe.length < 3)
+        {
+            $('#resultSearch').html('');
+            $('#resultSearch').removeClass('find');
+        }
+
        if(searchRecipe.length >= 3)
        {
            $.ajax({
-               url: '',
-               method: 'POST',
-               data:  ''
+               url: 'http://localhost/recipe-project/public/api.php/recipe/'+searchRecipe,
+               method: 'GET',
            })
-               .done(function (status, response) {
+               .done(function (data, status) {
+                   var recipe = JSON.parse(data);
+                   console.log(recipe.data.length);
+                   if(recipe.data != null)
+                   {
+                       for(var i in recipe.data)
+                       {
+                           console.log(recipe.data);
+                           $('#resultSearch').append("<div class='recipeResult recipeName' data-id='"+i+"'><img src='../assets/img/recipe.png'><span class='itemResult'>"+recipe.data[i].recipe_name+"</span></div>" +
+                               "<br />");
+                       }
+                   }
+                   else
+                   {
+
+                   }
 
                })
                .fail(function(status, response, error){
@@ -103,7 +128,66 @@ $(function() {
 
                });
        }
+
     });
+
+    $("#resultSearch").on('click','.recipeResult', function(event){
+        let id = $(this).data('id');
+
+        if(id != null)
+        {
+            $('#resultSearch').html('');
+
+            $.ajax({
+                url:'http://localhost/recipe-project/public/api.php/recipe/'+id,
+                method: 'GET',
+                dataType: 'json'
+            })
+            .done(function(data, status)
+            {
+                let result = JSON.parse(JSON.stringify(data));
+                console.log(result);
+
+                for(var i in result.data)
+                {
+                    $('#recipeSearch').append("<div id='general'></div>");
+                    $('#general').append("<div>"+result.data[i].recipe_name+"</div>");
+                    $('#general').append("<div>"+result.data[i].recipe_time+"</div>");
+                    $('#recipeSearch').append("<div id='ingredientList'></div>");
+                    for(var x in result.data[i].ingredient)
+                    {
+                        $('#ingredientList').append("<div>"+result.data[i].ingredient[x]+"</div>");
+                    }
+                }
+            })
+            .fail(function(data, status, error)
+            {
+
+            });
+        }
+    });
+
+    $('#recipeTab').on('click', '.editLine', function(e) {
+        let id = $(this).attr('id');
+        $('#name_'+id).show();
+        $("#"+id+" > .accordion > .recipeName").hide();
+        $('#time_'+id).show();
+        $("#"+id+" > .accordion > .recipeTime").hide();
+
+        $('#ingredient_'+id+' > .ingredient_list').each(function(i)
+        {
+            $(this).hide();
+        });
+
+        $('#ingredient_'+id+' > .ingredient_input > input').each(function(i)
+        {
+            $(this).show();
+        });
+
+        $('#commandLine_'+id).show();
+
+    });
+
 
 });
 
@@ -118,19 +202,20 @@ function loadRecipe()
     })
         .done(function (data, status) {
             var recipe = JSON.parse(data);
-            console.log(recipe.data);
 
             for(var i in recipe.data)
             {
                 $('#recipeTab').append("<div class='recipeRow' id='"+i+"'></div>");
                 $('#'+i).append("<div class='accordion'><input type='checkbox' id='accordion-"+i+"' name='accordion-checkbox' hidden></div>");
-                $("#"+i+" > .accordion").append("<div class='column col-2 recipeObject recipeName'><img src='../assets/img/recipe.png'>"+recipe.data[i].recipe_name+"</div><div class='column col-9 recipeObject recipeTime'><img src='../assets/img/alarm-clock.png'><b>"+recipe.data[i].recipe_time+"</b></div>");
-                $("#"+i+" > .accordion").append("<label class='accordion-header' for='accordion-"+i+"'><span class='openAccordion'><img src='../assets/img/add.png'></span></label>");
+                $("#"+i+" > .accordion").append("<div class='column col-2 recipeObject recipeName'><img src='../assets/img/recipe.png'>"+recipe.data[i].recipe_name+"</div><input id='name_"+i+"' type='text' value='"+recipe.data[i].recipe_name+"' hidden><div class='column col-9 recipeObject recipeTime'><img src='../assets/img/alarm-clock.png'><b>"+recipe.data[i].recipe_time+"</b></div><input id='time_"+i+"' type='text' value='"+recipe.data[i].recipe_time+"' hidden>");
+                $("#"+i+" > .accordion").append("<label class='accordion-header' for='accordion-"+i+"'><span class='openAccordion'><img src='../assets/img/add.png'></span><span id='"+i+"'class='editLine'><img src='../assets/img/edit.png'></span></label>");
                 $("#"+i+" > .accordion").append("<div class='accordion-body'></div>");
+                $("#"+i+" > .accordion > .accordion-body").append("<div id='ingredient_"+i+"'></div>");
                 for(var x in recipe.data[i].ingredient)
                 {
-                    $("#"+i+" > .accordion > .accordion-body").append("<span>"+recipe.data[i].ingredient[x]+"</span>");
+                    $("#ingredient_"+i).append("<span class='ingredient_list'>"+recipe.data[i].ingredient[x]+"</span><span class='ingredient_input'><input type='text' value='"+recipe.data[i].ingredient[x]+"' hidden></span>");
                 }
+                $("#"+i+" > .accordion > .accordion-body").append("<div hidden id='commandLine_"+i+"'><span><a class='updateLine'>Modifier</a></span> | <span><a class='backLine'>Annuler</a></span></div>");
             }
         })
         .fail(function(data, response, error){
--- views/recipe.php
+++ views/recipe.php
@@ -10,12 +10,6 @@
             <a id="addButton" class="btn btn-link nav-color-text">Add recipe</a>
             <a id="searchButton" class="btn btn-link nav-color-text">Search recipe</a>
         </section>
-        <section class="navbar-section nav-search">
-            <div class="input-group input-inline" id="searchBar">
-                <input class="form-input" type="text" placeholder="SEARCH RECIPE">
-                <button class="btn btn-primary input-group-btn btn-color"><span><img src="./../assets/img/search.png"></span></button>
-            </div>
-        </section>
     </header>
 
     <div class="container-box">
@@ -55,7 +49,7 @@
 
         </article>
 
-        <article class="column col-10 recipeBox">
+        <article id="searchBox" class="column col-10 recipeBox">
             <div class="headArticle">
                 <h3>Search recipe</h3>
                 <span id="logoRecipe"><img src="./../assets/img/loupe.png"</span>
@@ -67,6 +61,9 @@
                     <label class="form-label" for="nameRecipe">Search</label>
                     <div class="d-inline-flex max-width">
                         <input class="form-input" type="text" id="searchRecipe" placeholder="hamburger, pizza...">
+                    </div>
+                    <div id="resultSearch"></div>
+                    <div id="recipeSearch">
                     </div>
                 </div>
 
@@ -88,7 +85,6 @@
 
                 <div id="recipeTab" class="form-group">
                 </div>
-
             </div>
 
         </article>
